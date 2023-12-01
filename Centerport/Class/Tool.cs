32e0f4afd465ecba7d31883c6df44f5dcfc714b1
Koi git <96707504+koiy7010@users.noreply.github.com>
2023-12-01 11:49:29 +0800

    remove the logo from SEA_MLC report

--- Centerport/Class/Tool.cs
+++ Centerport/Class/Tool.cs
@@ -23,22 +23,22 @@ namespace MedicalManagementSoftware
             return str.Replace("'", "`").Replace(@"\", "\\");
         }
 
-        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
-        {
-            var ratioX = (double)maxWidth / image.Width;
-            var ratioY = (double)maxHeight / image.Height;
-            var ratio = Math.Min(ratioX, ratioY);
+        //public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        //{
+        //    var ratioX = (double)maxWidth / image.Width;
+        //    var ratioY = (double)maxHeight / image.Height;
+        //    var ratio = Math.Min(ratioX, ratioY);
 
-            var newWidth = (int)(image.Width * ratio);
-            var newHeight = (int)(image.Height * ratio);
+        //    var newWidth = (int)(image.Width * ratio);
+        //    var newHeight = (int)(image.Height * ratio);
 
-            var newImage = new Bitmap(newWidth, newHeight);
+        //    var newImage = new Bitmap(newWidth, newHeight);
 
-            using (var graphics = Graphics.FromImage(newImage))
-                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+        //    using (var graphics = Graphics.FromImage(newImage))
+        //        graphics.DrawImage(image, 0, 0, newWidth, newHeight);
 
-            return newImage;
-        }
+        //    return newImage;
+        //}
 
 
 
